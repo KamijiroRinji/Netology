a3142a853cea448576622f6660554a85fbd40325
KamijiroRinji <33496363+KamijiroRinji@users.noreply.github.com>
2019-10-16 18:10:57 +0300

    Create netology_adpy8_task_1.2.1.py

---
+++ netology_adpy8_task_1.2.1.py
@@ -0,0 +1,17 @@
+import json
+import wikipedia
+
+
+class GetWikiLink:
+    def __init__(self, path):
+        self.file = open(path, encoding="utf8")
+        self.countries = json.load(self.file)
+
+    def __iter__(self):
+        return ({country: wikipedia.page(country).url} for country in self.countries)
+
+
+if __name__ == "__main__":
+    countries = [country for country in GetWikiLink("countries.json")]
+    with open("countries.json", "w") as file_write:
+        json.dump(countries, file_write, ensure_ascii=False, indent=2)
